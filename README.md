dba72f8a3a1902b401b2c90041c979bbc7b97d11
Andrew Galyan-Mann <asgalyanmann@gmail.com>
2021-07-26 22:38:13 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Run App
+  * Open a terminal session at the parent directory "real-time-example"
+  * Run `npm i` to ensure all needed modules are installed
+  * Add your AssemblyAI Token to line 13 in `./js/server.js`
+  * Spin up the server to get your temporary auth token using `npm start`
+  * Open a new terminal at the parent directory and serve the project with `serve .`
---
+++ js/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get('/', async (request, res) => {
+  try {
+    const response = await axios.post('https://api.assemblyai.com/v2/realtime/token', // use account token to get a temp user token
+      { expires_in: 3600 }, // can set a TTL timer in seconds.
+      { headers: { authorization: 'YOUR-AAI-TOKEN' } }); // token
+    const { data } = response;
+    res.json(data);
+  } catch (error) {
+    res.json(`Error: ${error}`);
+  }
+});
+
+app.set('port', 5000);
+const server = app.listen(app.get('port'), () => {
+  console.log(`Server is running on port ${server.address().port}`);
+});
