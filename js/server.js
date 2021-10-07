4c552a37790b6ee915b5157317be75f29cd3b2b1
Andrew Galyan-Mann <asgalyanmann@gmail.com>
2021-10-07 15:59:09 -0400

    changer server port to 8000 to make room for serve

--- js/server.js
+++ js/server.js
@@ -10,7 +10,7 @@ app.get('/', async (req, res) => {
   try {
     const response = await axios.post('https://api.assemblyai.com/v2/realtime/token', // use account token to get a temp user token
       { expires_in: 3600 }, // can set a TTL timer in seconds.
-      { headers: { authorization: 'YOUR-API-TOKEN' } }); // token
+      { headers: { authorization: 'YOUR-AAI-API-KEY' } }); // token
     const { data } = response;
     res.json(data);
   } catch (error) {
@@ -18,7 +18,7 @@ app.get('/', async (req, res) => {
   }
 });
 
-app.set('port', 5000);
+app.set('port', 8000);
 const server = app.listen(app.get('port'), () => {
   console.log(`Server is running on port ${server.address().port}`);
 });
