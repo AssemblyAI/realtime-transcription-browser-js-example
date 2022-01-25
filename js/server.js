70a0ce4cd2b373978539764685f8b7c110ad8593
Andrew Galyan-Mann <57883097+agalyanmann@users.noreply.github.com>
2022-01-25 10:38:35 -0500

    update packages, ports

--- js/server.js
+++ js/server.js
@@ -10,7 +10,7 @@ app.get('/', async (req, res) => {
   try {
     const response = await axios.post('https://api.assemblyai.com/v2/realtime/token', // use account token to get a temp user token
       { expires_in: 3600 }, // can set a TTL timer in seconds.
-      { headers: { authorization: 'YOUR-AAI-API-KEY' } }); // AssemblyAI API Key goes here
+      { headers: { authorization: '3ee77ccd12f44bcbb15795cf697797d0' } }); // AssemblyAI API Key goes here
     const { data } = response;
     res.json(data);
   } catch (error) {
@@ -19,7 +19,7 @@ app.get('/', async (req, res) => {
   }
 });
 
-app.set('port', 5000);
+app.set('port', 8000);
 const server = app.listen(app.get('port'), () => {
   console.log(`Server is running on port ${server.address().port}`);
 });
