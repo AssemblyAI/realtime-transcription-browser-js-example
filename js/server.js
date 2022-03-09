48ee7d1d58a646acc17cea152f425288767a8d7b
Andrew Galyan-Mann <57883097+agalyanmann@users.noreply.github.com>
2022-03-09 15:15:08 -0500

    Update server.js

--- js/server.js
+++ js/server.js
@@ -10,7 +10,7 @@ app.get('/', async (req, res) => {
   try {
     const response = await axios.post('https://api.assemblyai.com/v2/realtime/token', // use account token to get a temp user token
       { expires_in: 3600 }, // can set a TTL timer in seconds.
-      { headers: { authorization: '3ee77ccd12f44bcbb15795cf697797d0' } }); // AssemblyAI API Key goes here
+      { headers: { authorization: YOUR API TOKEN HERE } }); // AssemblyAI API Key goes here
     const { data } = response;
     res.json(data);
   } catch (error) {
