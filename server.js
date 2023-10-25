6f888971fb3be297c0e81b931e78f9f01c085d26
Niels Swimberghe <3382717+Swimburger@users.noreply.github.com>
2023-10-25 17:16:07 -0400

    Use SDK

---
+++ server.js
@@ -0,0 +1,20 @@
+const { AssemblyAI } = require("assemblyai");
+const express = require("express");
+const path = require("path");
+
+const aai = new AssemblyAI({ apiKey: "54f6b610676245a4b928f1061fce8d4f" })
+const app = express();
+app.use(express.static('public'))
+app.use('/assemblyai.js', express.static(path.join(__dirname, 'node_modules/assemblyai/dist/index.umd.js')))
+app.use(express.json());
+
+app.get("/token", async (_req, res) => {
+  const token = await aai.realtime.createTemporaryToken({ expires_in: 3600 });
+  res.json({ token });
+});
+
+app.set("port", 8000);
+const server = app.listen(app.get("port"), () => {
+  console.log(`Server is running on port ${server.address().port}`);
+});
+
