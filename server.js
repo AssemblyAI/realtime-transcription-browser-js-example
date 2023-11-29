cf237301f938fd9a37df991f60b88220b555edd6
Niels Swimberghe <3382717+Swimburger@users.noreply.github.com>
2023-11-29 16:15:34 -0500

    Use AAI SDK

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-const { AssemblyAI } = require("assemblyai");
+require('dotenv').config()
 const express = require("express");
 const path = require("path");
+const { AssemblyAI } = require("assemblyai");
 
-const aai = new AssemblyAI({ apiKey: "YOUR_API_KEY" })
+const aai = new AssemblyAI({ apiKey: process.env.ASSEMBLYAI_API_KEY })
 const app = express();
 app.use(express.static('public'))
-app.use('/assemblyai.js', express.static(path.join(__dirname, 'node_modules/assemblyai/dist/index.umd.js')))
+app.use('/assemblyai.js', express.static(path.join(__dirname, 'node_modules/assemblyai/dist/assemblyai.umd.js')))
 app.use(express.json());
 
 app.get("/token", async (_req, res) => {
@@ -15,6 +16,5 @@ app.get("/token", async (_req, res) => {
 
 app.set("port", 8000);
 const server = app.listen(app.get("port"), () => {
-  console.log(`Server is running on port ${server.address().port}`);
+  console.log(`Server is running on port http://localhost:${server.address().port}`);
 });
-
